2f24897751fb4e5b32dac239add1538c34eecfaf
John_pels <ajeigbejohnolu@gmail.com>
2023-02-20 15:34:17 +0100

    feat: add create, get, update, and delete update handlers

--- src/handlers/update.ts
+++ src/handlers/update.ts
@@ -25,6 +25,74 @@ export const getUpdates = async (req: Request, res: Response) => {
   }, []);
   res.json({ data: updates });
 };
-export const createUpdate = async (req: Request, res: Response) => {};
-export const updateUpdate = async (req: Request, res: Response) => {};
-export const deleteUpdate = async (req: Request, res: Response) => {};
+export const createUpdate = async (req: Request, res: Response) => {
+  const product = await prisma.product.findUnique({
+    where: {
+      id: req.body.productId,
+    },
+  });
+  if (!product) {
+    //doest not belong to the user
+    return res.json({ message: "It does not belong to the user" });
+  }
+  const update = await prisma.update.create({
+    data: {
+      title: req.body.title,
+      body: req.body.body,
+      product: { connect: { id: product.id } },
+    },
+  });
+  res.json({ data: update });
+};
+export const updateUpdate = async (req: Request, res: Response) => {
+  const products = await prisma.product.findMany({
+    where: {
+      belongsToId: req.user.id,
+    },
+    include: {
+      Update: true,
+    },
+  });
+  const updates = products.reduce((allUpdates: any, product) => {
+    return [...allUpdates, ...product.Update];
+  }, []);
+  const match = updates.find((update: any) => update.id === req.params.id);
+  if (!match) {
+    res.json({ message: "Not found" });
+  }
+
+  const updatedUpdate = await prisma.update.update({
+    where: {
+      id: req.params.id,
+    },
+    data: req.body,
+  });
+
+  res.json({ data: updatedUpdate });
+};
+
+export const deleteUpdate = async (req: Request, res: Response) => {
+  const products = await prisma.product.findMany({
+    where: {
+      belongsToId: req.user.id,
+    },
+    include: {
+      Update: true,
+    },
+  });
+  const updates = products.reduce((allUpdates: any, product) => {
+    return [...allUpdates, ...product.Update];
+  }, []);
+
+  const match = updates.find((update: any) => update.id === req.params.id);
+
+  if (!match) {
+    res.json({ message: "Not found" });
+  }
+  const deleted = await prisma.update.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.json({ data: deleted });
+};
--- src/router.ts
+++ src/router.ts
@@ -8,6 +8,13 @@ import {
   getProducts,
   updateProduct,
 } from "./handlers/product";
+import {
+  createUpdate,
+  deleteUpdate,
+  getOneUpdate,
+  getUpdates,
+  updateUpdate,
+} from "./handlers/update";
 
 const router = Router();
 //Product
@@ -28,24 +35,24 @@ router.post(
 router.delete("/product/:id", deleteProduct);
 
 //Update
-router.get("/update", () => {});
-router.get("/update/:id", () => {});
+router.get("/update", getUpdates);
+router.get("/update/:id", getOneUpdate);
 router.put(
   "/update/:id",
   body("title").optional(),
   body("body").optional(),
-  body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]),
+  body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]).optional(),
   body("version").optional(),
-  () => {}
+  updateUpdate
 );
 router.post(
   "/update",
   body("title").exists().isString(),
   body("body").exists().isString(),
   body("productId").exists().isString(),
-  () => {}
+  createUpdate
 );
-router.delete("/update/:id", () => {});
+router.delete("/update/:id", deleteUpdate);
 
 //Update Point
 router.get("/updatepoint", () => {});
