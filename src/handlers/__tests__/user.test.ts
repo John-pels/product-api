df327469e3db01b41ba5f49b68b3e4a2e88a3909
John_pels <ajeigbejohnolu@gmail.com>
2023-03-06 13:08:26 +0100

    feat: integrate testing for create new user route

--- src/handlers/__tests__/user.test.ts
+++ src/handlers/__tests__/user.test.ts
@@ -1,5 +1,13 @@
+import * as user from "../user";
+
 describe("user handler", () => {
-  it("should do something when something happens", () => {
-    expect(1).toBe(1);
+  it("should create a new user", async () => {
+    const req: any = { body: { username: "hello", password: "hiiiiiyi" } };
+    const res: any = {
+      json({ token }: any) {
+        expect(token).toBeTruthy();
+      },
+    };
+    await user.createNewUser(req, res, () => {});
   });
 });
